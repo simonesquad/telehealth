a8688e5d4c3cc4a2640dd17708f6eb9c0c315b0c
Simone Ballard <simone.a.ballard@gmail.com>
2024-04-19 17:36:40 -0500

    set up backend server

---
+++ server/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectToDatabase = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        const connect = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUriParser: true,
+        });
+        console.log(`MongoDb Connected: ${connect.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
+};
