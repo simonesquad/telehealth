2f2185e4620a3ed565effb200820000456e1cf88
Simone Ballard <simone.a.ballard@gmail.com>
2024-04-20 16:04:11 -0500

    server bootstrapped but connection string hardcoded

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectToDatabase = async () => {
     try {
         mongoose.set('strictQuery', false)
-        const connect = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNewUriParser: true,
-        });
+        const connect = await mongoose.connect("mongodb+srv://simoneaballard:ptOADHVogJtfrhjg@cluster0.ly974xk.mongodb.net/telehealth");
         console.log(`MongoDb Connected: ${connect.connection.host}`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
