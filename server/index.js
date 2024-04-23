654c30158b1453b5ae8203fa5783e59e6b1a8f0c
Simone Ballard <simone.a.ballard@gmail.com>
2024-04-23 18:54:01 -0500

    api call to get data worked

--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ import connectToDatabase from './db.js';
 import express from 'express';
 import cors from 'cors';
 
+// Routes
+import productRoutes from './routes/productRoutes.js';
+
 connectToDatabase();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
+app.use('/api/products', productRoutes);
+
+// localhost:5000/api/products
 
 const port = 5000;
 
