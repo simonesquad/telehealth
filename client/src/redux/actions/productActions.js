811485cdec9725252792db13b052e73b470b97c9
Simone Ballard <simone.a.ballard@gmail.com>
2024-05-13 20:51:18 -0500

    updated store

---
+++ client/src/redux/actions/productActions.js
@@ -0,0 +1,23 @@
+import { setProducts, setLoading, setError, setPagination } from '../slices/product';
+import axios from 'axios';
+
+
+export const getProducts = (page, favoriteToggle) => async (dispatch) => {
+    dispatch(setLoading())
+    try {
+        const { data } = await axios.get(`/api/products`)
+        const { products, pagination } = data;
+        dispatch(setProducts(products))
+        dispatch(setPagination(pagination));
+    } catch (error) {
+        dispatch(
+            setError(
+                error.response && error.response.data.message 
+                ? error.response.data.message 
+                : error.message 
+                ? error.message
+                : 'An unexpected error has occurred. Please try again later.'
+            )
+        );
+    }
+};
