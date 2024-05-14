1e258f99de98fe130ce2387992843434fe321ad1
Simone Ballard <simone.a.ballard@gmail.com>
2024-05-13 19:47:02 -0500

    updated products screen

--- client/src/screens/ProductsScreen.js
+++ client/src/screens/ProductsScreen.js
@@ -1,4 +1,4 @@
-import { Box, Wrap } from '@chakra-ui/react';
+import { Box, Wrap, WrapItem, Center } from '@chakra-ui/react';
 import ProductCard from '../components/ProductCard';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -22,7 +22,14 @@ const ProductsScreen = () => {
             {data.length > 1 && (
                 <Box>
                     <Wrap spacing='30px' justify='center' minHeight='80vh' mx={{ base: '12', md: '20', lg: '32'}}>
-                        <ProductCard product={data[0]} loading={false} />
+                        {/* <ProductCard product={data[0]} loading={false} /> */}
+                        {data.map((product) => (
+                            <WrapItem key={product._id}>
+                                <Center w='250px' h='450px'>
+                                    <ProductCard product={product} loading={false} />
+                                </Center>
+                            </WrapItem>
+                        ))}
                     </Wrap>
                 </Box>
             )}
