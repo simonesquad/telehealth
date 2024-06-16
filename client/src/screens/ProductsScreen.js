ee87a606a8136f557cecb77505383a58e04742ab
Simone Ballard <simoneballard@Simones-MacBook-Pro.local>
2024-06-16 15:28:37 -0500

    switched to redux on products screen

--- client/src/screens/ProductsScreen.js
+++ client/src/screens/ProductsScreen.js
@@ -2,28 +2,36 @@ import { Box, Wrap, WrapItem, Center } from '@chakra-ui/react';
 import ProductCard from '../components/ProductCard';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProducts } from '../redux/actions/productActions';
 
 const ProductsScreen = () => {
-    const [data, setData] = useState([]);
+    const dispatch = useDispatch();
+    const { loading, error, products, pagination } = useSelector((state) => state.product);
 
     useEffect(() => {
-        axios
-            .get('/api/products')
-            .then((response) => {
-                setData(response.data.products);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-    }, []);
+        dispatch(getProducts());
+}, [dispatch]);
+
+    // const [data, setData] = useState([]);
+
+    // useEffect(() => {
+    //     axios
+    //         .get('/api/products')
+    //         .then((response) => {
+    //             setData(response.data.products);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error fetching data:', error);
+    //     });
+    // }, []);
 
     return (
         <> 
-            {data.length > 1 && (
+            {products.length > 1 && (
                 <Box>
                     <Wrap spacing='30px' justify='center' minHeight='80vh' mx={{ base: '12', md: '20', lg: '32'}}>
-                        {/* <ProductCard product={data[0]} loading={false} /> */}
-                        {data.map((product) => (
+                        {products.map((product) => (
                             <WrapItem key={product._id}>
                                 <Center w='250px' h='450px'>
                                     <ProductCard product={product} loading={false} />
