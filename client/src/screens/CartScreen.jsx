5377785cbdc65a0601835aa931a07b175fdce669
Simone Ballard <simoneballard@Simones-MacBook-Pro.local>
2024-09-19 19:39:33 -0500

    updated cart screen

--- client/src/screens/CartScreen.jsx
+++ client/src/screens/CartScreen.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import {
+    Box,
+    Flex,
+    Heading,
+    HStack,
+    Link,
+    Stack,
+    useColorModeValue as mode,
+    Spinner,
+    Alert,
+    AlertIcon,
+    AlertDescription,
+    Wrap,
+    AlertTitle,
+} from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import CartItem from '../components/CartItem';
+import OrderSummary from '../components/OrderSummary';
 
-function CartScreen() {
-  return (
-    <div>CartScreen</div>
-  )
-}
+const CartScreen = () => {
+    const { loading, error, cartItems } = useSelector((state) => state.cart);
 
-export default CartScreen
+    const getHeadingContent = () => (cartItems.length === 1 ? '(1 Item)' : `(${cartItems.length} Items)`);
+
+    return (
+        <Wrap spacing='30px' justify='center' minHeight='100vh'>
+            {loading ? (
+                <Stack direction='row' spacing='4'> <Spinner mt='20' thickness='2px' speed='0.65s' emptyColor='gray.200' color='cyan.500' size='xl' /></Stack> 
+            ) : error ? (
+                <Alert status='error'>
+                <AlertIcon />
+                <AlertTitle>We are sorry!</AlertTitle>
+                <AlertDescription>{error}</AlertDescription>
+              </Alert>
+            ) : cartItems.length <= 0 ? (
+                <Alert status='warning'>
+                <AlertIcon />
+                <AlertTitle>Your cart is empty.</AlertTitle>
+                <AlertDescription>
+                    <Link as={ReactLink} to='/products'>
+                        Click here to see your products.
+                    </Link>
+                </AlertDescription>
+              </Alert>
+            ) : (
+                <p>display cart</p>
+            )}
+        </Wrap>
+    );
+};
+
+export default CartScreen;
