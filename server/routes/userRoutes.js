3ad487d2502682b9b85fa7b70d3371f2e50515b8
Simone Ballard <simone.a.ballard@gmail.com>
2024-10-10 12:20:29 -0500

    updates

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -78,13 +78,46 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 // verify Email
+const verifyEmail = asyncHandler( async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1]
+    try {
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+        const user = await User.findById(decoded.id);
+
+        if(user) {
+            user.active = true;
+            await user.save()
+            res.json('Thanks for activating your account. You can close this window now.');
+        } else {
+            res.status(404).send('User not found.');
+        }
+    } catch (error) {
+        res.status(401).send('Email address could not be verified.')
+    }
+})
 
 // password reset request
+const passwordResetRequest = asyncHandler(async(req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email: email });
+
+        if(user) {
+            const newToken = genToken(user._id);
+            sendPassswordResetEmail(newToken, user.email, user.name, user._id);
+            res.status(200).send(`We sent you a recovery email to ${email}`);
+        }
+    } catch (error) {
+        res.status(401).send('There is not an account with such an email address.')
+    }
+})
 
 // password reset
 
 
 userRoutes.route('/login').post(loginUser);
 userRoutes.route('/register').post(registerUser);
+userRoutes.route('/verify-email').get(verifyEmail);
 
 export default userRoutes;
