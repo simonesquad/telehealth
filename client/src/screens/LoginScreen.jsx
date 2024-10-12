63ec702f5f13ac5cac94203ebd36a451cd1a82c5
Simone Ballard <simone.a.ballard@gmail.com>
2024-10-12 18:07:33 -0500

    added second part of form - need to check imports

--- client/src/screens/LoginScreen.jsx
+++ client/src/screens/LoginScreen.jsx
@@ -11,7 +11,9 @@ import {
     Heading,
     Stack,
     Text,
-    useToast
+    useToast,
+    useBreakpointValue,
+
 
 } from '@chakra-ui/react';
 import { Formik } from 'formik';
@@ -22,15 +24,20 @@ import * as Yup from 'yup';
 import PasswordField from '../components/PasswordField';
 import PasswordForgottenForm from '../components/PasswordForgottenForm';
 import TextField from '../components/TextField';
+import { login } from '../redux/actions/userActions';
 
 const LoginScreen = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const redirect = '/products';
     const toast = useToast();
+    const dispatch = useDispatch();
 
     const { loading, error, userInfo, message } = useSelector((state) => state.user);
     const [ showPasswordReset, setShowPasswordReset ] = useState(false);
+
+    const headingBR = useBreakpointValue({ base: 'xs', md: 'sm' });
+    const boxBR = useBreakpointValue({ base: 'transparent', md: 'bg-surface' });
 
     useEffect(() => {
         if(userInfo) {
@@ -56,7 +63,70 @@ const LoginScreen = () => {
         }
     }, [userInfo, redirect, error, navigate, location.state, toast, showPasswordReset, message]);
 
-  return <div>LoginScreen</div>;
+    return (
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={Yup.object({
+            email: Yup.string().email('Invalid email.').required('An email address is required.'),
+            password: Yup.string()
+              .min(1, 'Password is too short - must contain at least 1 character.')
+              .required('Password is required.'),
+          })}
+          onSubmit={(values) => {
+            dispatch(login(values.email, values.password));
+          }}>
+          {(formik) => (
+            <Container maxW='lg' py={{ base: '12', md: '24' }} px={{ base: '0', md: '8' }} minH='4xl'>
+              <Stack spacing='8'>
+                <Stack spacing='6'>
+                  <Stack spacing={{ base: '2', md: '3' }} textAlign='center'>
+                    <Heading size={headingBR}>Log in to your account</Heading>
+                    <HStack spacing='1' justify='center'>
+                      <Text color='muted'>Don't have an account ?</Text>
+                      <Link to='/registration'>
+                      <Button variant='link' colorScheme='orange'>
+                        Sign up
+                      </Button>
+                      </Link> 
+                    </HStack>
+                  </Stack>
+                </Stack>
+                <Box
+                  py={{ base: '0', md: '8' }}
+                  px={{ base: '4', md: '10' }}
+                  bg={{ boxBR }}
+                  boxShadow={{ base: 'none', md: 'xl' }}>
+                  <Stack spacing='6' as='form' onSubmit={formik.handleSubmit}>
+                    {error && (
+                      <Alert
+                        status='error'
+                        flexDirection='column'
+                        alignItems='center'
+                        justifyContent='center'
+                        textAlign='center'>
+                        <AlertIcon />
+                        <AlertTitle>We are sorry!</AlertTitle>
+                        <AlertDescription>{error}</AlertDescription>
+                      </Alert>
+                    )}
+                    <Stack spacing='5'>
+                      <FormControl>
+                        <TextField type='text' name='email' placeholder='you@example.com' label='Email' />
+                        <TextField type='password' name='password' placeholder='your password' label='Password' />
+                      </FormControl>
+                    </Stack>
+                    <Stack spacing='6'>
+                      <Button colorScheme='orange' size='lg' fontSize='md' isLoading={loading} type='submit'>
+                        Sign in
+                      </Button>
+                    </Stack>
+                  </Stack>
+                </Box>
+              </Stack>
+            </Container>
+          )}
+        </Formik>
+      );
 };
 
 export default LoginScreen;
