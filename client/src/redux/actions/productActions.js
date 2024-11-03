0a121d91708ae70a1fb57504e26ca76767caa83f
Simone Ballard <simone.a.ballard@gmail.com>
2024-11-03 16:39:44 -0600

    updated product reviewed

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,4 +1,4 @@
-import { setProducts, setLoading, setError, setPagination, setFavorites, setFavoritesToggle, setProduct } from '../slices/product';
+import { setProducts, setLoading, setError, setPagination, setFavorites, setFavoritesToggle, setProduct, productReviewed } from '../slices/product';
 import axios from 'axios';
 
 
@@ -74,3 +74,25 @@ export const getProduct = (id) => async (dispatch) => {
         );
     }
 };
+
+export const createProductReview = (productId, userId, comment, rating, title) => async (dispatch, getState) => {
+    const {
+        user: {userInfo}
+    } = getState()
+    try {
+        const config = { headers: { Authorization: `Bearer ${userInfo.token}`, 'Content-Type': 'application/json' } };
+
+        await axios.post(`/api/products/reviews/${productId}`, { comment, userId, rating, title }, config);
+        dispatch(productReviewed(true));
+    } catch (error) {
+        dispatch(
+            setError(
+                error.response && error.response.data.message 
+                ? error.response.data.message 
+                : error.message 
+                ? error.message
+                : 'An unexpected error has occurred. Please try again later.'
+            )
+        );
+    }
+}
