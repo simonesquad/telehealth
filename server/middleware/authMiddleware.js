a955acc0d9fb5d3bbdde38f9a51d910fe4799b50
Simone Ballard <simone.a.ballard@gmail.com>
2024-11-03 15:59:58 -0600

    updated product routes

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ import User from '../models/User.js';
 const protectRoute = asyncHandler(async (req, res, next) => {
     let token;
 
-    if (req.headers.authorization && res.headers.authorization.startsWith('Bearer')) {
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
@@ -17,6 +17,11 @@ const protectRoute = asyncHandler(async (req, res, next) => {
             res.status(401);
             throw new Error('Not authorized, token failed.');
         }
+    }
+
+    if(!token) {
+        res.status(401)
+        throw new Error('Not authorized, no token.');
     }
 });
 
