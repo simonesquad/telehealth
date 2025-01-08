f9165e0296c9d525156a71e11a9aa30f280f9f17
Simone Ballard <simone.a.ballard@gmail.com>
2025-01-07 18:40:32 -0800

    updated cancel screen

--- client/src/screens/CancelScreen.jsx
+++ client/src/screens/CancelScreen.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Center, Text, Box, Button } from '@chakra-ui/react';
+import { BsBoxSeamFill } from 'react-icons/bs';
+import { Link as ReactLink } from 'react-router-dom';
 
 const CancelScreen = () => {
   return (
-    <div>CancelScreen</div>
-  )
-}
+    <Center height='100vh' flexDirection='column'>
+      <Text fontSize={{basse: 'md', md: 'xl', lg: '4xl' }}>You have canceled the payment process.</Text>
+      <Box m='2'>
+        <BsBoxSeamFill size='50px' mt='2' />
+      </Box>
 
-export default CancelScreen
+      <Text>But we have saved your cart, just in case.</Text>
+      <Button as={ReactLink} to='/cart' mt='2'>
+        Go to your cart
+      </Button>
+    </Center>
+  );
+};
+
+export default CancelScreen;
