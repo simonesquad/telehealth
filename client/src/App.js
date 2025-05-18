1764366ff55c75cc3d830ef7c58a2d1ef2048bac
Simone Ballard <simone.a.ballard@gmail.com>
2025-05-18 14:58:51 -0700

    imported extend theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import Header from './components/Header';
 // screens //
